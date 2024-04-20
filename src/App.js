c57b97350bc9edcd436c51613fa15b3efceea8c5
Aditya Deshmukh <a_mdeshmukh@ch.iitr.ac.in>
2024-04-21 00:59:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div class="container">
+    <div class="l-container">
+      <h1 class="l-title">Login Page</h1>
+      <form class="form">
+        <div class="input-group ">
+          <label for="username">Username</label>
+          <input type="username" name="username" id="username" />
+          
+        </div>
+
+        <div class="input-group ">
+          <label for="password">Password</label>
+          <input type="password" name="password" id="password" />
+          
+        </div>
+
+        <button type="submit" class="login-button">Login</button>
+      </form>
+    </div>
+  </div>
     </div>
   );
 }
